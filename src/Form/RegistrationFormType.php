ac884fc33c3afab96b1104eb7990d9750a4e8b41
YohannHommet <yohann.hommet@outlook.fr>
2021-08-26 20:04:39 +0200

    Update style on register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -27,16 +27,25 @@ class RegistrationFormType extends AbstractType
         // adding constraints 
         $builder
             ->add('firstname', TextType::class, [
-                'label' => 'Prénom',
+                'label' => 'Prénom *',
                 'required' => true,
+                'attr' => [
+                    'placeholder' => 'ex : Jean'
+                ]
             ])
             ->add('lastname', TextType::class, [
-                'label' => 'Nom de famille',
+                'label' => 'Nom de famille *',
                 'required' => true,
+                'attr' => [
+                    'placeholder' => 'ex : Dupont'
+                ]
             ])
             ->add('nickname', TextType::class, [
-                'label' => 'Pseudo',
+                'label' => 'Pseudo *',
                 'required' => true,
+                'attr' => [
+                    'placeholder' => 'ex : jd'
+                ]
             ])
             // ajout d'un évènement sur le formulaire pour différencier l'édition de la création d'un utilisateur
             // mapped false pour le password afin de ne pas relier à l'entité en cas de modification de password
@@ -61,10 +70,19 @@ class RegistrationFormType extends AbstractType
                             'format' => 'ddMMMMyyyy'
                         ])
                         ->add('city', TextType::class, [
-                            'label' => 'Ville',
+                            'label' => 'Ville *',
                             'required' => true,
+                            'attr' => [
+                                'placeholder' => 'ex : Paris'
+                            ]
                         ])
-                        ->add('email', EmailType::class)
+                        ->add('email', EmailType::class, [
+                            'label' => 'Adresse email *',
+                            'required' => true,
+                            'attr' => [
+                                'placeholder' => 'ex : email@exemple.fr'
+                            ]
+                        ])
                         ->add('password', RepeatedType::class, [
                             'type' => PasswordType::class,
                             'invalid_message' => 'Les mots de passe ne correspondent pas.',
@@ -75,11 +93,11 @@ class RegistrationFormType extends AbstractType
                                     new NotCompromisedPassword(),
                                     new NotBlank(),
                                 ],
-                                'label' => 'Mot de passe',
+                                'label' => 'Mot de passe *',
                                 'help' => 'Minimum huit caractères, une lettre, un chiffre et un caractère spécial.'
                             ],
                             'second_options' => [
-                                'label' => 'Répéter le mot de passe',
+                                'label' => 'Répéter le mot de passe *',
                                 'constraints' => [
                                     new NotBlank(),
                                 ],
