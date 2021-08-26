1f5aa404b59341b4f0cb1caa018ffe59fc60da84
Keygen9 <azerodrigues.daniel@gmail.com>
2021-08-26 16:01:54 +0200

    comment form with interactive buttons ok

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', function () {
 })  
 
 // Auto close Flashmessage after 6sec
-const flashMessage = document.querySelector('#flash_message')
+const flashMessage = document.querySelector('#flash_messages')
 if (flashMessage != null) {
     setTimeout(() =>
     {
@@ -18,4 +18,27 @@ if (flashMessage != null) {
 if (window.location.pathname == '/') {
     document.querySelector('#q').style.height = '2.5rem'
 }
+
+// toggle comment box
+const targetDiv = document.querySelector(".create__comment");
+const btn = document.getElementById("toggle__comments");
+btn.onclick = function () {
+    if (targetDiv.classList.contains("create__comment")) {
+    targetDiv.classList.replace("create__comment", "create__comment_toggle")
+    btn.style.display = "none"
+    } else {
+    targetDiv.classList.replace("create__comment_toggle", "create__comment")
+    }
+};
+
+//cancel comment
+const cancelbtn = document.getElementById("cancel__comment");
+cancelbtn.onclick = function () {
+    if (targetDiv.classList.contains("create__comment_toggle")) {
+        targetDiv.classList.replace("create__comment_toggle", "create__comment")
+        btn.style.display = "block"
+    } 
+};
+
+
 
