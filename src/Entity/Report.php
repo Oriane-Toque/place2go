a48a0a4734b3116d78524011586c21a3f20b7f1e
Oriane-Toque <oriane.toque@gmail.com>
2021-08-27 11:04:01 +0200

    wip - report entity created

---
+++ src/Entity/Report.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\Timestamps;
+use App\Repository\ReportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReportRepository::class)
+ * @ORM\Table(name="report")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Report
+{
+		use Timestamps;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
