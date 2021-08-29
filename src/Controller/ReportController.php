3fcf1f30520328c76082d14a21934754bff9ab3d
Oriane-Toque <oriane.toque@gmail.com>
2021-08-29 18:35:05 +0200

    wip - method to report an event done

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Entity\Report;
 use App\Entity\User;
 use App\Form\ReportType;
@@ -23,7 +24,6 @@ class ReportController extends AbstractController
 	{
 		// vérifie si l'auteur est connecté
 		$this->denyAccessUnlessGranted("USER_ACCESS", $this->getUser(), "Requirements not met");
-
 
 		// création d'un nouveau signalement
 		$report = new Report;
@@ -48,7 +48,57 @@ class ReportController extends AbstractController
 
             $this->addFlash('success', 'Votre rapport a bien été envoyé aux modérateurs ! Et sera traité dans les plus brefs délais');
 
-            return $this->redirect($request->headers->get('referer'));
+            return $this->redirectToRoute('app_profile_show', ['id' => $user->getId()]);
+        }
+
+				$this->addFlash('danger', 'Vous avez déjà signalé cet utilisateur !');
+
+				return $this->redirectToRoute('app_profile_show', ['id' => $user->getId()]);
+		}
+
+		return $this->render('report/report.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
+
+		/**
+	 * Signalement d'un utilisateur
+	 *
+	 * @Route("/report/event/{id<\d+>}", name="app_report_event", methods={"GET", "POST"})
+	 */
+	public function event(Request $request, Event $event, EntityManagerInterface $em, ReportRepository $reportRepository)
+	{
+		// vérifie si l'auteur est connecté
+		$this->denyAccessUnlessGranted("USER_ACCESS", $this->getUser(), "Requirements not met");
+
+		// création d'un nouveau signalement
+		$report = new Report;
+
+		// récupération de l'utilisateur qui signale
+		$author = $this->getUser();
+		// utilisateur de la sortie signalé
+		$user = $event->getAuthor();
+
+		// création du formulaire
+		$form = $this->createForm(ReportType::class, $report);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+
+			$report->setAuthor($author);
+			// utilisateur signalé
+			$report->setUser($user);
+			// sortie de l'utilisateur signalé
+			$report->setEvent($event);
+			
+        if (!$reportRepository->findOneBy(["event" => $event, "author" => $author])) {
+
+            $em->persist($report);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre rapport a bien été envoyé aux modérateurs ! Et sera traité dans les plus brefs délais');
+
+            return $this->redirectToRoute('app_profile_show', ['id' => $user->getId()]);
         }
 
 				$this->addFlash('danger', 'Vous avez déjà signalé cet utilisateur !');
