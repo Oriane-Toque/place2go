0201ad897a331512613ca1ca86d09e90d7f9374e
Oriane-Toque <oriane.toque@gmail.com>
2021-08-30 16:16:47 +0200

    win - contact form + email send + carbon copy for user

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController {
@@ -17,16 +20,34 @@ class MainController extends AbstractController {
 	 * @Route("/contact", name="app_contact_contact", methods={"GET", "POST"})
 	 * @return Response
 	 */
-	public function contact(Request $request): Response {
+	public function contact(Request $request, MailerInterface $mailer): Response {
 
 		$user = $this->getUser();
 		$this->denyAccessUnlessGranted('USER_ACCESS', $user, 'Requirements not met');
 
 		$form = $this->createForm(ContactType::class);
-		$form->handleRequest($request);
+		$contact = $form->handleRequest($request);
 
 		if($form->isSubmitted() && $form->isValid()) {
-			// todo
+
+			// on crée le mail
+			$email = (new TemplatedEmail())
+				->from(new Address($user->getEmail()))
+				->to(new Address('checkmyapplications@gmail.com', 'Place 2 Go Emailer'))
+				->subject('Place2Go - '.$contact->get('subject')->getData())
+				->cc(new Address($user->getEmail()))
+				->htmlTemplate('contact/contact_email.html.twig')
+				->context([
+					'user' => $user,
+					'subject' => $contact->get('subject')->getData(),
+					'message' => $contact->get('message')->getData(),
+				]);
+			// on envoie le mail
+			$mailer->send($email);
+
+			$this->addFlash('success', 'Votre email a bien été envoyé et sera traité dans les plus brefs délais !');
+			
+			return $this->redirectToRoute('app_home');
 		}
 
 		return $this->render("contact/contact.html.twig", [
