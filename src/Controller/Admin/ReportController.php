822fc6748a9e7c043c80c321bbb99946f3be2ba4
Oriane-Toque <oriane.toque@gmail.com>
2021-08-31 15:46:46 +0200

    wip - method to change status & delete a report done

--- src/Controller/Admin/ReportController.php
+++ src/Controller/Admin/ReportController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Report;
 use App\Repository\ReportRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +17,7 @@ class ReportController extends AbstractController {
 	 * Liste les signalements en cours de traitement
 	 *
 	 * @Route("/admin/reports", name="admin_report_list", methods={"GET"})
+	 * @param ReportRepository $rr
 	 * @return Response
 	 */
 	public function list(ReportRepository $rr): Response {
@@ -30,6 +35,7 @@ class ReportController extends AbstractController {
 	 * Liste les signalements traités/archivés
 	 *
 	 * @Route("/admin/reports/archive", name="admin_report_archive", methods={"GET"})
+	 * @param ReportRepository $rr
 	 * @return Response
 	 */
 	public function archive(ReportRepository $rr): Response {
@@ -41,5 +47,40 @@ class ReportController extends AbstractController {
 		return $this->render("admin/report/list.html.twig", [
 			'reports' => $reports,
 		]);
+	}
+
+	/**
+	 * Change le statut d'un signalement (en cours/traité)
+	 *
+	 * @Route("/admin/reports/{id}/status", name="admin_report_status", methods={"GET"})
+	 * @param Report $report
+	 * @param EntityManagerInterface $em
+	 * @param Request $request
+	 * @return RedirectResponse
+	 */
+	public function process(Report $report, EntityManagerInterface $em, Request $request): RedirectResponse {
+
+		if($report->getStatus() === false){
+			$report->setStatus(true);
+		} else {
+			$report->setStatus(false);
+		} 
+
+		$em->flush();
+
+		return $this->redirect($request->headers->get('referer'));
+	}
+
+	/**
+	 * Supprimer un signalement
+	 *
+	 * @Route("/admin/report/{id<\d+>}/delete", name="admin_report_delete", methods={"GET"})
+	 */
+	public function delete(Report $report, EntityManagerInterface $em, Request $request) {
+
+		$em->remove($report);
+		$em->flush();
+
+		return $this->redirect($request->headers->get('referer'));
 	}
 }
