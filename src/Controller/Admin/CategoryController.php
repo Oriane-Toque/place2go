2feb42954d00dc9fd4e6cfc9b1bff4cc860a4de6
Tigrou1664 <tigrou1664@live.fr>
2021-08-31 03:01:12 +0200

    Add isGranted on all admin classes

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -11,6 +11,13 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * Require ROLE_ADMIN for *every* controller method in this class.
+ *
+ * @IsGranted("ROLE_ADMIN")
+ */
 
 class CategoryController extends AbstractController
 {
